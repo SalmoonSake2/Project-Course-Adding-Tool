a4dafed7d9d1dbca6c6e378d84471a962c5105a6
SalmoonSake2 <159425758+SalmoonSake2@users.noreply.github.com>
2024-12-09 20:48:08 +0800

    Update run.py
    maybe this can fix the problem in Mac platform?

--- src/run.py
+++ src/run.py
@@ -10,7 +10,7 @@ Salmoon Sake
 程序的入口，請從這邊開始執行。
 '''
 import os
-os.chdir(os.path.dirname(os.path.dirname(__file__)))
+os.chdir(os.path.abspath(os.path.join(__file__, "../..")))
 
 from libs.interface.app import APP
 
