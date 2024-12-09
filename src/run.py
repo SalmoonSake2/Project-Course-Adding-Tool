4af4a00ff772b17878f8100c5c1689740df1a044
Salmoon Sake <eberthoyl@gmail.com>
2024-12-09 11:54:55 +0800

    更新路徑問題

--- src/run.py
+++ src/run.py
@@ -22,10 +22,15 @@ ttkbootstrap: https://github.com/israel-dryer/ttkbootstrap/
 已知問題：
 - 結果畫面有時無法順利滾動，這是由於scrollframe被物件遮掩所致，滑鼠移一下位置就好了。
 - 希望老師能給A+
+
+備註：需要根據requirements 安裝所需的包
 -------------------------------------------------------------------------------
 
 程序的入口，請從這邊開始執行。
 '''
+import os
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
 from libs.interface.app import APP
 
 APP()
