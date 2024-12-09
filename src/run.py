1c6219c2ab7c45af595367c200d4f7f1aa8a83b7
Salmoon Sake <eberthoyl@gmail.com>
2024-12-09 21:34:09 +0800

    maybe fix theplatform diff... again

--- src/run.py
+++ src/run.py
@@ -10,6 +10,9 @@ Salmoon Sake
 程序的入口，請從這邊開始執行。
 '''
 import os
+import sys
+
+sys.path.append("../")
 os.chdir(os.path.abspath(os.path.join(__file__, "../..")))
 
 from libs.interface.app import APP
